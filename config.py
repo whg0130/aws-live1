75b774fc3f66456c22685f497b2ca72cb9453ffa
whg0130 <62163330+whg0130@users.noreply.github.com>
2023-12-20 08:38:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cqhbhwdtq8wu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hauguan-employee"
+customregion = "us-east-1"
+
